456d7f24910d6aeae701dcf3c775df0b1e0d1d50
40325068 <adam.dovciak@gmail.com>
2019-03-08 16:06:16 +0000

    Edit design

--- app/src/main/java/AdamDovciak/napier/ac/uk/MainActivity.java
+++ app/src/main/java/AdamDovciak/napier/ac/uk/MainActivity.java
@@ -1,13 +1,31 @@
 package AdamDovciak.napier.ac.uk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainGame();
+            }
+        });
+    }
+
+    public void openMainGame(){
+        Intent gameView = new Intent(this, ActivityTwo.class);
+        startActivity(gameView);
     }
 }
